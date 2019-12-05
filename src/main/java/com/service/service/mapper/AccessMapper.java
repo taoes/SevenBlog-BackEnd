eee3e786648c5c2ae85ac06bb7850fe63257bd75
周涛 <zhoutaotao@aliyun.com>
2019-12-05 09:29:31 +0800

    feat: 访问量新增按访问次数 排序

--- src/main/java/com/service/service/mapper/AccessMapper.java
+++ src/main/java/com/service/service/mapper/AccessMapper.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.service.service.mapper.dao.AccessDO;
 import com.service.service.mapper.dao.KeyValue;
 import java.util.List;
-import java.util.Map;
 import org.apache.ibatis.annotations.Select;
 
 public interface AccessMapper extends BaseMapper<AccessDO> {
@@ -12,7 +11,8 @@ public interface AccessMapper extends BaseMapper<AccessDO> {
   @Select({
     "SELECT access.area AS name, COUNT(1) AS value",
     "FROM access",
-    "GROUP BY access.area",
+    "GROUP BY access.area ",
+    "ORDER BY COUNT(1) DESC"
   })
   List<KeyValue> count();
 }
